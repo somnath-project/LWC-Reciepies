757d12a2a5997da3dc70b862950a1826fdaba1ba
“mahesht-mrk” <mahesh.thakresfdc@gmail.com>
2024-12-13 10:32:24 +0530

    @api Decorater Parent to child push by Mahesh

---
+++ force-app/main/default/lwc/assignment1ParentDataShow/assignment1ParentDataShow.js
@@ -0,0 +1,25 @@
+import { LightningElement, wire } from 'lwc';
+import getcontactList from '@salesforce/apex/ContactListDetails.getContactList';
+import Phone from '@salesforce/schema/Account.Phone';
+export default class Assignment1ParentDataShow extends LightningElement {
+    
+
+
+    con = [
+        {Name: 'Mahesh', Email: 'Mahesh@gmail.com', Phone: 68746826 },
+        {Name: 'Vinod', Email: 'Vinpod@gmail.com ', Phone: 61234567890 },
+        {Name: 'Somnath', Email: 'Somnath@gmail.com', Phone: 9876543234567 }
+    ];
+    
+
+    // @wire(getcontactList)
+    // wiredContacts({ error, data }) {
+    //     if (data) {
+    //         this.contacts = data;
+    //     } else if (error) {
+    //         this.error = error;
+    //     }
+   // }
+
+
+}
